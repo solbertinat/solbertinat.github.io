30c629ae3fd9dc5d537ce856d25c8b52c0a3e4fd
solbertinat <102978663+solbertinat@users.noreply.github.com>
2026-02-13 11:01:03 -0300

    Update cursor.js

--- assets/js/cursor.js
+++ assets/js/cursor.js
@@ -3,6 +3,6 @@ cursor.className = "custom-cursor";
 document.body.appendChild(cursor);
 
 document.addEventListener("mousemove", (e) => {
-  cursor.style.left = `${e.clientX}px`;
-  cursor.style.top = `${e.clientY}px`;
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
 });
